aa154bd25d4b83753fe9e913c991d26099049d93
vocoderism <davide.franzino@gmail.com>
2014-06-10 22:23:16 +0200

    CRoMize

--- crom.mk
+++ crom.mk
@@ -2,11 +2,13 @@
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common stuff.
-$(call inherit-product, vendor/slim/config/gsm.mk)
 
-# Inherit some common stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+# Inherit CRoM common stuff.
+$(call inherit-product, vendor/crom/config/common_phone.mk)
+
+# bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/crom/prebuilt/common/media/xhdpi/BOOTANIMATION-1280x768.zip:system/media/bootanimation.zip
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/maguro/full_maguro.mk)
@@ -20,7 +22,7 @@ PRODUCT_RELEASE_NAME := maguro
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.2.2/JDQ39/573038:user/release-keys"
 PRIVATE_BUILD_DESC="yakju-user 4.2.2 JDQ39 573038 release-keys"
 
-PRODUCT_NAME := slim_maguro
+PRODUCT_NAME := crom_maguro
 PRODUCT_DEVICE := maguro
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Galaxy Nexus
